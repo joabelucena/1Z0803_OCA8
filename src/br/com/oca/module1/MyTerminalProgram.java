4ad3beb96289f05b1f503b22c24684bea4e9a636
Joabe Lucena <jlucena@ttrans.com.br>
2015-06-25 17:27:14 -0300

    Module1: Terminal Program

---
+++ src/br/com/oca/module1/MyTerminalProgram.java
@@ -0,0 +1,23 @@
+package br.com.oca.module1;
+
+/**
+ * 
+ * @author Joabe Lucena
+ * 
+ * @version 1.3 Create executable Java applications with a main method; run a Java program from the command line; including console output.
+ * 
+ * Steps:
+ * 
+ * 1) Create an file.java with a main class;
+ * 2) run 'javac file.java' command 
+ * 3) go to root path (/bin/ directory at 'org' or 'br' up level)
+ * 4) run 'java -cp . br.com.oca.module1.MyTerminalProgram'
+ *
+ */
+
+public class MyTerminalProgram {
+	public static void main(String[] args){
+		System.out.println("Hi this my terminal program!");
+	}
+
+}
